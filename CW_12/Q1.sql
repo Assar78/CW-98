8273d7e1bb3422d648180c674a13b1480600ebea
Hosein Assar <hosein.assar78@gmail.com>
2023-06-22 15:32:15 +0330

    number of films between 60 and 68

--- CW_12/Q1.sql
+++ CW_12/Q1.sql
@@ -3,3 +3,21 @@
 SELECT title, category, release_year FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category on film_category.category_id = category.category_id;
+
+SELECT title, category.name, release_year FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category on film_category.category_id = category.category_id
+WHERE category.name IN ('Action', 'Comedy', 'Family');
+
+SELECT category.name, COUNT(*) as category_count FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category on film_category.category_id = category.category_id
+GROUP BY category.name;
+
+SELECT category.name, COUNT(*) as category_count FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category on film_category.category_id = category.category_id
+GROUP BY category.name
+HAVING COUNT(*) BETWEEN 60 AND 68;
+
+ 
