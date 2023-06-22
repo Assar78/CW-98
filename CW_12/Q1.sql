e7ed0462fa47974917bc6792a89ec03bc869a08b
Hosein Assar <hosein.assar78@gmail.com>
2023-06-22 16:42:44 +0330

    final commit

--- CW_12/Q1.sql
+++ CW_12/Q1.sql
@@ -20,7 +20,14 @@ JOIN category on film_category.category_id = category.category_id
 GROUP BY category.name
 HAVING COUNT(*) BETWEEN 60 AND 68;
 
-SELECT title, category.name as language FROM film
+SELECT title, category.name AS category, language.name AS language
+FROM film
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category on film_category.category_id = category.category_id
-JOIN language ON language.language_id = file.language_id;
+JOIN language ON language.language_id = film.language_id;
+
+SELECT customer.customer_id,first_name,last_name,title,(return_date - rental_date) AS rental_duration
+FROM customer 
+JOIN rental ON customer.customer_id = rental.customer_id
+JOIN inventory ON inventory.inventory_id = rental.inventory_id
+JOIN film ON film.film_id = inventory.film_id;
